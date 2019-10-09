772e51750efca361c48c83bda2a90049141c3c73
Gustavo Gois <gustavo.gois@gmail.com>
2019-10-09 22:59:48 +0100

    Initial commit

---
+++ src/main/java/gois/study/umyspring5recipeapp/UmySpring5RecipeAppApplication.java
@@ -0,0 +1,13 @@
+package gois.study.umyspring5recipeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UmySpring5RecipeAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UmySpring5RecipeAppApplication.class, args);
+    }
+
+}
